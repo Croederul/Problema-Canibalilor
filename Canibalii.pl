c336e260c537e8e72f1bc281df90466201ae8dd1
Croederul <mihneacapabil@gmail.com>
2026-03-27 21:19:27 +0200

    stare + in_siguranta + traverseaza
    Prima parte de cod - stare + verificare daca misionarii sunt in siguranta pe ambele maluri + definirea tuturor mutarilor posibile (barca) + logica de traversare cu verificarile si calculele necesare

--- Canibalii.pl
+++ Canibalii.pl
@@ -1,6 +1,24 @@
-weather(buzau, summer, hot).
-weather(baicoi, summer, warm).
-weather(buzau, winter, cold).
-weather(baicoi, winter, warm).
-test test test()
-frfrsfseee oj feuysfume8 yap_flag(uipf suipey sfpiesf, Value)
+% stare(CanibaliStg, CanibaliDr, MisionariStg, MisionariDr, PozitieBarca) :-
+    verifica_mal(Misionari, Canibali) :- Misionari = 0.
+    verifica_mal(Misionari, Canibali) :- Misionari >= Canibali.
+    in_siguranta(stare(CanibaliStg, CanibaliDr, MisionariStg, MisionariDr, PozitieBarca)) :- 
+        verifica_mal(MisionariStg, CanibaliStg),
+        verifica_mal(MisionariDr, CanibaliDr).
+
+    % barca(Canibali, Misionari)
+    barca(1, 0),
+    barca(2, 0),
+    barca(0, 1),
+    barca(0, 2),
+    barca(1, 1).
+
+    traversare(stare(CanibaliStg, CanibaliDr, MisionariStg, MisionariDr, stg), stare(CanibaliStgNou, CanibaliDrNou, MisionariStgNou, MisionariDrNou, dr)) :-
+        barca(CanibaliBarca, MisionariBarca),
+        CanibaliStg >= CanibaliBarca,
+        MisionariStg >= MisionariBarca,
+        CanibaliStgNou is CanibaliStg - CanibaliBarca,
+        CanibaliDrNou is CanibaliDr + CanibaliBarca,
+        MisionariStgNou is MisionariStg - MisionariBarca,
+        MisionariDrNou is MisionariDr + MisionariBarca,
+        in_siguranta(stare(CanibaliStgNou, CanibaliDrNou, MisionariStgNou, MisionariDrNou, dr)).
+    
